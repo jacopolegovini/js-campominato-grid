50e65d2daea7a89605770509a21465852779fae0
jacopolegovini <legovini96@gmail.com>
2024-08-23 15:32:49 +0200

    milestone 3 completed

--- script.js
+++ script.js
@@ -6,24 +6,33 @@
 
 
 // Prendi gli elementi dal DOM
-const form = document.querySelector('form');
-let table = document.querySelector('.table');
+const formElement = document.querySelector('form');
+let tableElement = document.querySelector('.table');
 
 
 // Crea le variabili necessarie
 const numberCell = 100;
-let cell;
+
 
 
 // Imposta l'event listener
-form.addEventListener('click', function(event) {
+formElement.addEventListener('click', function(event) {
     event.preventDefault();
     
     for (let i = 0; i < numberCell; i++) {
-        cell = document.createElement('div');
-        cell.classList.add('cell');
-        table.appendChild(cell)
+        // Dichiara un array cells
+        let cells = [i];
+
+        // Monta quanto necessario
+        cells[i] = document.createElement('div');
+        cells[i].classList.add('cell');
+        tableElement.appendChild(cells[i]);
+        
+        // Richiama dal DOM quanto appena creato
+        const cellsElement = document.querySelectorAll('.cell'); 
+
+        // Monta quanto necessario
+        cellsElement[i].innerHTML += `<p>${i}</p>`;
+        console.log(cellsElement)
     }
-
-
 })
